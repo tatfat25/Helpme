da1b3d3a02cd527a65ba82f3c2b4f8c6c63a383f
huca06001 <huca06001@E-PW05C37R>
2024-10-09 12:10:00 +0200

    Add project files.

---
+++ Helpme/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Helpme
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favorit bok");
+            String Boknamn = Console.ReadLine();
+
+            Console.WriteLine("Det här är din favorit bok" + " " + Boknamn);
+
+            Console.ReadKey();
+        }
+    }
+}
+
